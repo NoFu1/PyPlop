b9389ad13fa2cc37e3d59ae86b6a05ab4e37bdcc
flavio <flavio@strongswan.ch>
2023-05-14 19:58:36 +0200

    added some mock config

--- plops.py
+++ plops.py
@@ -1,6 +1,13 @@
 from playsound import playsound
-
 class plops:
+    def __init__(self, overwrite_print = False):
+        self.ressourses = "plops/"
+        self.overwrite_print = overwrite_print
+
     def plop(self, plop: str):
-        playsound('plops/' + plop)
+        try:
+            playsound(self.ressources + plop)
+        except: #TODO make something usefull with the except...
+            print("file probably not found")
+
 
