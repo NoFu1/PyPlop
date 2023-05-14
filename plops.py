907876455acab102b5e1fd82ca8927e89fe5d1bf
Mauro Sbicego <mauro@sbicego.ch>
2023-05-14 19:46:08 +0200

    Renamed to plops

---
+++ plops.py
@@ -0,0 +1,6 @@
+from playsound import playsound
+
+class plops:
+    def plop(self, plop: str):
+        playsound('plops/' + plop)
+
--- sounds.py
+++
@@ -1,6 +0,0 @@
-from playsound import playsound
-
-class sounds:
-    def play(self, sound: str):
-        playsound('sounds/' + sound)
-
